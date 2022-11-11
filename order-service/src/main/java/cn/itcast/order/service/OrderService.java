58b7fe4c35d3b8add9cadb59983f9b049ef350d8
chu <2254886767@qq.com>
2022-11-11 16:16:11 +0800

    finish learning Feign

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,8 +1,9 @@
 package cn.itcast.order.service;
 
+import cn.itcast.feign.client.UserClient;
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,12 +17,17 @@ public class OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserClient userClient;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        /*String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);*/
+
+        User user = userClient.findById(order.getUserId());
 
         order.setUser(user);
         // 4.返回
